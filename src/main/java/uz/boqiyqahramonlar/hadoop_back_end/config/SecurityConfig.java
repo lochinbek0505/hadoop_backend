d7fb48f386a23ff0247ff59e5c2ce65f26ccd7e5
yengilmas <lbek458@gmail.com>
2026-04-22 10:34:26 +0500

    spring security update

---
+++ src/main/java/uz/boqiyqahramonlar/hadoop_back_end/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package uz.boqiyqahramonlar.hadoop_back_end.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .cors(Customizer.withDefaults())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .requestMatchers("/**").permitAll()
+                .anyRequest().authenticated()
+            );
+
+        return http.build();
+    }
+}
